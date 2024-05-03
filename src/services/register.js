76f47cc9d75ff044e11175bfbfad9460e43f7d91
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2024-05-03 17:16:59 +0300

    Catch register errors before database update

--- src/services/register.js
+++ src/services/register.js
@@ -1,5 +1,6 @@
 import { UserModel } from "../models/user.model.js";
 import bcrypt from "bcrypt";
+import { registerValidation } from "../middleware/registerValidation.js";
 
 export const registerService = async (data) => {
   try {
@@ -15,6 +16,11 @@ export const registerService = async (data) => {
     } = data;
 
     const hashedPassword = await bcrypt.hash(password, 10);
+
+    const { error } = registerValidation.validate(data);
+    if (error) {
+      throw new Error(error.details[0].message);
+    }
 
     const newUser = new UserModel({
       firstName,
