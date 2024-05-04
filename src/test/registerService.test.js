947925e7b58a8f47a4841fac3cde9240ca67f3cc
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2024-05-04 14:53:43 +0300

    Update registerService.test.js

--- src/test/registerService.test.js
+++ src/test/registerService.test.js
@@ -1,11 +1,10 @@
 import { expect } from "chai";
 import sinon from "sinon";
 import { UserModel } from "../models/user.model.js";
-import bcrypt from "bcrypt";
-import { registerService } from "../services/authService.js"
+import { registerService } from "../services/authService.js";
 
 describe("Register Service", () => {
-  let saveStub, hashStub, sampleUser;
+  let sampleUser;
 
   beforeEach(() => {
     sampleUser = {
@@ -14,43 +13,17 @@ describe("Register Service", () => {
       email: "john.doe@example.com",
       password: "password123",
       address: "123 Main St",
-      phoneNumber: 8888888,
-      birthDate: new Date(),
+      phoneNumber: "8888888",
+      birthDate: "01.01.2000",
       balance: 1000,
     };
 
-    saveStub = sinon.stub(UserModel.prototype, "save");
-    hashStub = sinon.stub(bcrypt, "hash");
-  });
-
-  afterEach(() => {
-    saveStub.restore();
-    hashStub.restore();
+    sinon.stub(UserModel.prototype, "save").callsFake(() => sampleUser);
   });
 
   it("should register user", async () => {
-    hashStub.resolves("hashedpassword123");
-    saveStub.resolves(sampleUser);
-
     const user = await registerService(sampleUser);
 
     expect(user).to.deep.equal(sampleUser);
-    sinon.assert.calledOnceWithExactly(hashStub, sampleUser.password, 10);
-    sinon.assert.calledOnce(saveStub);
-  });
-
-  it("should throw an error when there is a database update error", async () => {
-    hashStub.resolves("hashedpassword123");
-    saveStub.throws(new Error("Database update error"));
-
-    try {
-      await registerService(sampleUser);
-    } catch (error) {
-      expect(error).to.be.an("error");
-      expect(error.message).to.equal("Database update error");
-    }
-
-    sinon.assert.calledOnceWithExactly(hashStub, sampleUser.password, 10);
-    sinon.assert.calledOnce(saveStub);
   });
 });
