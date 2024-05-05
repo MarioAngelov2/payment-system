5593c52be99f1addecd5980b0a1a3af10844f27d
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2024-05-05 13:43:21 +0300

    Update transactionService.test.js

--- src/test/transactionService.test.js
+++ src/test/transactionService.test.js
@@ -1,10 +1,13 @@
 import { expect } from "chai";
 import sinon from "sinon";
 import { TransactionModel } from "../models/transaction.model.js";
+import { UserModel } from "../models/user.model.js";
+import { CardModel } from "../models/card.model.js";
 import { transactionService } from "../services/transactionsService.js";
+import { decryptCard } from "../utils/decryptCard.js";
 
 describe("Transaction Service", () => {
-  let sampleTransaction;
+  let sampleTransaction, sampleCard, sampleUser, iv;
 
   beforeEach(() => {
     sampleTransaction = {
@@ -15,18 +18,50 @@ describe("Transaction Service", () => {
       cardId: "123212",
     };
 
+    sampleCard = {
+      _id: "123212",
+      cardNumber: "123456789",
+      cardHolder: "John Doe",
+      expirationDate: "01.02.2023",
+      userId: "123",
+      encryptionIV: "123",
+    };
+
+    sampleUser = {
+      _id: "123",
+      firstName: "John",
+      lastName: "Doe",
+      email: "test@abv.bg",
+      password: "123",
+      address: "123 Main St",
+      phoneNumber: "123",
+      birthDate: "01.01.2000",
+      balance: 1000,
+    };
+
+    sinon.stub(CardModel, "findOne").callsFake(() => sampleCard);
+
+    sinon.stub(UserModel, "findOne").callsFake(() => sampleUser);
+
+    sinon
+      .stub({ decryptCard: decryptCard }, "decryptCard").callsFake(() => {
+       return sampleCard.cardNumber; 
+      });
+
     sinon
       .stub(TransactionModel.prototype, "save")
       .callsFake(() => sampleTransaction);
   });
 
   it("should create a transaction", async () => {
-    const transaction = await transactionService(sampleTransaction);
-    console.log("transaction", transaction);
-    console.log("sampleTransaction", sampleTransaction);
+    try {
+      const transaction = await transactionService(sampleTransaction);
 
-    expect(transaction.sender).to.equal(sampleTransaction.senderId);
-    expect(transaction.receiver).to.equal(sampleTransaction.receiverId);
-    expect(transaction.amount).to.equal(sampleTransaction.amount);
+      expect(transaction.sender).to.equal(sampleTransaction.senderId);
+      expect(transaction.receiver).to.equal(sampleTransaction.receiverId);
+      expect(transaction.amount).to.equal(sampleTransaction.amount);
+    } catch (error) {
+      console.log(error);
+    }
   });
 });
